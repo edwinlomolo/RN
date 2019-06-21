80334cab5924902d0e018e4a71327209693374a6
3dw1nM0535 <workockmoses@gmail.com>
2019-06-21 18:10:57 +0300

    feat(add/delete/check todo)

--- App.js
+++ App.js
@@ -7,13 +7,69 @@
  */
 
 import React, { Component } from 'react';
-import { View, StyleSheet, Text } from 'react-native';
+import { View, ScrollView, StyleSheet, Text, Button } from 'react-native';
+
+let id = 0;
+
+const Todo = props => (
+  <View>
+    <Text>{props.todo.text}</Text>
+    <Button onPress={props.onDelete} title="delete" />
+  </View>
+)
 
 export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [],
+    }
+  }
+
+  addTodo() {
+    id++;
+    const text = `TODO number ${id}`;
+    this.setState({
+      todos: [ ...this.state.todos,
+        { id: id, text: text, checked: false },
+      ]
+    })
+  }
+
+  removeTodo(id) {
+    this.setState({
+      todos: this.state.todos.filter(todo => todo.id !== id),
+    })
+  }
+
+  toggleTodo(id) {
+    this.setState({
+      todos: this.state.todos.map(todo => {
+        if (todo.id !== id) return todo;
+        return {
+          id: todo.id,
+          text: todo.text,
+          checked: !todo.checked
+        };
+      }),
+    });
+  }
   render() {
     return (
-      <View style={styles.container}>
-        <Text>Welcome to React Native App Tutorials</Text>
+      <View>
+        <Text>Todo count: {this.state.todos.length}</Text>
+        <Text>Unchecked todos: {this.state.todos.filter(
+          todo => !todo.checked).length}</Text>
+        <Button onPress={() => this.addTodo()} title="Add Todo" />
+        <ScrollView>
+          {this.state.todos.map(todo => (
+            <Todo
+              toggleChange={() => this.toggleTodo(todo.id)}
+              onDelete={() => this.removeTodo(todo.id)}
+              todo={todo}
+            />
+          ))}
+        </ScrollView>
       </View>
     )
   }
@@ -23,6 +79,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    justifyContent: "center"
+    justifyContent: "center",
+    backgroundColor: "#ecf0f1"
   }
 });
